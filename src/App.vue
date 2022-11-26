a133e2327a242b4041f05dff9a0171612f657f83
Santiago Caci <santiago.caci@gmail.com>
2022-11-25 22:16:24 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const counter = ref<number>(0);
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Hola</h1>
+    <h2>Mundo</h2>
+    <p>Counter</p>
+    <button @click="counter++">+1</button>
+    {{ counter }}
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+</style>
